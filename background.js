9278007cb1034f8d12af48e39c2630cb09e4d99e
Keshav Kumar <KeshavKumar@MacBook-Air.local>
2025-12-26 00:25:54 +0530

    Added the ui to the extension view

--- background.js
+++ background.js
@@ -1,12 +1,6 @@
-(() => {
-  let youtubeLeftCotrol, youtubePlayer;
-  let curretVideo = '';
-
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    const { type, videoId } = request;
-    if (type === 'NEW') {
-      curretVideo = videoId;
-      newVideoLoaded();
-    }
-  });
-})();
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && tab.url && tab.url.includes('youtube.com/watch')) {
+    const params = new URLSearchParams(tab.url.split('?')[1] || '');
+    chrome.tabs.sendMessage(tabId, { type: 'NEW', videoId: params.get('v') });
+  }
+});
